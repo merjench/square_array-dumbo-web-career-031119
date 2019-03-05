688d5bd68c51864eb04b8379d3bb0dbbffe184f9
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-05 23:35:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
-def square_array(array)
-  # your code here
-  new_number = []
-  array.each do |element|
-    new_number<<(n ** 2)
-  end 
-  return new_number
-  end 
-
+def square_array(numbers)
+  # your code here	  
+  new_numbers = []
+  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
+end
